afbae955a55ac6a2b76c93e1bc861a764ea234fc
Divyanshkhare-06 <divyanshkhare656@gmail.com>
2025-03-06 10:22:08 +0530

    Update delatspecific.c

--- lab 4/delatspecific.c
+++ lab 4/delatspecific.c
@@ -1,15 +1,9 @@
-// C program to delete a linked list node at a given
-// position
-
 #include <stdio.h>
 #include <stdlib.h>
-
-// Node structure for the linked list
 struct Node {
     int data;
     struct Node* next;
 };
-
 struct Node* newNode(int data)
 {
     struct Node* node
@@ -18,32 +12,21 @@ struct Node* newNode(int data)
     node->next = NULL;
     return node;
 }
-
-// Function to delete a node at a given position
 struct Node* deleteNode(struct Node* head, int position)
 {
     struct Node* temp = head;
     struct Node* prev = NULL;
-
-    // Base case if linked list is empty
     if (temp == NULL)
         return head;
-
-    // Case 1: Head is to be deleted
     if (position == 1) {
         head = temp->next;
         free(temp);
         return head;
     }
-
-    // Case 2: Node to be deleted is in middle
-    // Traverse till given position
     for (int i = 1; temp != NULL && i < position; i++) {
         prev = temp;
         temp = temp->next;
     }
-
-    // If given position is found, delete node
     if (temp != NULL) {
         prev->next = temp->next;
         free(temp);
@@ -51,10 +34,8 @@ struct Node* deleteNode(struct Node* head, int position)
     else {
         printf("Data not present\n");
     }
-
     return head;
 }
-
 void printList(struct Node* head)
 {
     while (head != NULL) {
@@ -63,8 +44,6 @@ void printList(struct Node* head)
     }
     printf("NULL\n");
 }
-
-// Driver code
 int main()
 {
     struct Node* head = newNode(1);
@@ -72,22 +51,16 @@ int main()
     head->next->next = newNode(3);
     head->next->next->next = newNode(4);
     head->next->next->next->next = newNode(5);
-
     printf("Original list: ");
     printList(head);
-
     int position = 2;
     head = deleteNode(head, position);
-
     printf("List after deletion: ");
     printList(head);
-
-    // Cleanup remaining nodes
     while (head != NULL) {
         struct Node* temp = head;
         head = head->next;
         free(temp);
     }
-
     return 0;
 }
