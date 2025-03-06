0bf1b128c48e201cccf347b1de8e5ca5edd62d2a
Divyanshkhare-06 <divyanshkhare656@gmail.com>
2025-03-06 10:19:50 +0530

    Update delatbegin.c

--- lab 4/delatbegin.c
+++ lab 4/delatbegin.c
@@ -1,6 +1,3 @@
-// C program to delete the head node
-// from a linked list
-
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -8,29 +5,15 @@ struct Node {
     int data;
     struct Node* next;
 };
-
-// Delete the head node and return the new head
-struct Node* deleteHead(struct Node* head) {
-  
-    // Check if the list is empty
+struct Node* deleteHead(struct Node* head) { 
     if (head == NULL)
         return NULL;
-
-    // Store the current head in a 
-      // temporary variable
     struct Node* temp = head;
-
-    // Move the head pointer to the next node
     head = head->next;
-
-    // Free the memory of the old head node
     free(temp);
-
     return head;
 }
-
 void printList(struct Node* curr) {
-      
     while (curr != NULL) {
         printf("%d ", curr->data);
         curr = curr->next;
@@ -43,11 +26,7 @@ struct Node *createNode(int new_data){
     new_node->next = NULL;
     return new_node;
 }
-
 int main() {
-  
-    // Create a hard-coded linked list:
-    // 3 -> 12 -> 15 -> 18
     struct Node* head = createNode(3);
     head->next = createNode(12);
     head->next->next = createNode(15);
