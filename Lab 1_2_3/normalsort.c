fa3e21238971016f731620053dbeb08066a67257
Divyanshkhare-06 <divyanshkhare656@gmail.com>
2025-04-06 03:21:21 +0530

    Create normalsort.c

---
+++ Lab 1_2_3/normalsort.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int main (){
+    int n,flag,temp;
+    printf("enter the size of array: ");
+    scanf("%d",&n);
+    int a[n];
+    printf("Enter the elements of array");
+    for(int i=0;i<n;i++){
+        scanf("%d",&a[i]);
+    }
+    for(int i=0;i<n-1;i++){
+        flag=0;
+        for(int j=0;j<n-i-1;j++){
+            if(a[j]>a[j+1]){
+               temp=a[j];
+               a[j]=a[j+1];
+               a[j+1]=temp;
+                flag =1;
+            }
+        }
+        if(flag==0)
+        break;
+    }
+    for(int i=0;i<n;i++){
+        printf("%d\n",a[i]);
+    }
+    return 0;
+}
